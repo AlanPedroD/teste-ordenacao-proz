5a53289ec218f0707bda61b70676663c6279f17f
AlanPedroD <alandiaslp@hotmail.com>
2023-03-03 20:25:01 -0300

    desafio ordenacao proz

---
+++ script.js
@@ -0,0 +1,17 @@
+function pulaLinha() {
+    document.write("<br>");
+}
+
+function mostra(frase) {
+    document.write(frase);
+    pulaLinha();
+}
+
+var equipe = prompt('Digite o nome da sua equipe:')
+
+var multiplicador = 1;
+
+while(multiplicador <= 5){
+    mostra("<h3>" + equipe + " - " + multiplicador + "</h3>");
+    multiplicador = multiplicador + 1;
+}
