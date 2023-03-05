8b29ba5f8de86c1af620114b35bfd02f0d656f7d
AlanPedroD <alandiaslp@hotmail.com>
2023-03-05 08:43:22 -0300

    Acrescentei um estilo

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function pulaLinha() {
-    document.write("<br>");
-}
+// function pulaLinha() {
+//     document.write("<br>");
+// }
 
-function mostra(frase) {
-    document.write(frase);
-    pulaLinha();
-}
+// function mostra(frase) {
+//     document.write(frase);
+//     pulaLinha();
+// }
 
-var equipe = prompt('Digite o nome da sua equipe:')
+// var equipe = prompt('Digite o nome da sua equipe:')
 
-var multiplicador = 1;
+// var multiplicador = 1;
 
-while(multiplicador <= 5){
-    mostra("<h3>" + equipe + " - " + multiplicador + "</h3>");
-    multiplicador = multiplicador + 1;
-}
+// while(multiplicador <= 5){
+//     mostra("<h3>" + equipe + " - " + multiplicador + "</h3>");
+//     multiplicador = multiplicador + 1;
+// }
